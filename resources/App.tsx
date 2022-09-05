7287ea5d1f8924c5dbe64d0826292538c874d32c
SuperCheese21 <ethan.shields21@gmail.com>
2022-09-04 17:18:48 -0700

    Added typography plugin

--- resources/App.tsx
+++ resources/App.tsx
@@ -1,7 +1,10 @@
 export const App = (): JSX.Element => (
-  <>
-    <button className="btn btn-primary">Button</button>
-  </>
+  <div className="flex h-screen">
+    <div className="m-auto">
+      <div className="text-6xl text-red-600">1</div>
+      <button className="btn">Button</button>
+    </div>
+  </div>
 );
 
 export default App;
