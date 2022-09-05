848f3adaed35c6105b5411f5eea4678eb6ef0998
SuperCheese21 <ethan.shields21@gmail.com>
2022-09-04 17:21:59 -0700

    Added react-daisyui

--- resources/App.tsx
+++ resources/App.tsx
@@ -1,8 +1,10 @@
+import { Button } from 'react-daisyui';
+
 export const App = (): JSX.Element => (
   <div className="flex h-screen">
     <div className="m-auto">
       <div className="text-6xl text-red-600">1</div>
-      <button className="btn">Button</button>
+      <Button color="primary">Click me!</Button>
     </div>
   </div>
 );
