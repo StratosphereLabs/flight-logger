9a3f544a1224a6918f7464693dba7c8100602aec
SuperCheese21 <ethan.shields21@gmail.com>
2022-09-07 22:58:13 -0700

    Added footer and stats

--- resources/layouts/MainLayout/MainLayout.tsx
+++ resources/layouts/MainLayout/MainLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import { MainFooter } from './MainFooter';
 import { MainNavbar } from './MainNavbar';
 
 export const MainLayout = (): JSX.Element => (
   <>
     <MainNavbar />
     <Outlet />
+    <MainFooter />
   </>
 );
