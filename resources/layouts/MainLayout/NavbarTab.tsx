2c4a388b40e9f6fe7ef6fd6fc37c72ba86c7604a
SuperCheese21 <ethan.shields21@gmail.com>
2023-02-10 11:17:41 -0800

    Added link to my flights

--- resources/layouts/MainLayout/NavbarTab.tsx
+++ resources/layouts/MainLayout/NavbarTab.tsx
@@ -1,18 +1,29 @@
 import classNames from 'classnames';
+import { ReactNode } from 'react';
+import { Menu } from 'react-daisyui';
 import { NavLink, NavLinkProps } from 'react-router-dom';
 
 export interface NavbarTabProps extends NavLinkProps {
   className?: string;
+  subMenu?: ReactNode;
 }
 
 export const NavbarTab = ({
   className,
+  subMenu,
   ...props
 }: NavbarTabProps): JSX.Element => (
-  <NavLink
-    {...props}
-    className={({ isActive }) =>
-      classNames('tab tab-lg', isActive && 'tab-active', className)
-    }
-  />
+  <Menu.Item>
+    <NavLink
+      {...props}
+      className={({ isActive }) =>
+        classNames(
+          'rounded-lg',
+          isActive && ['bg-primary', 'text-white'],
+          className,
+        )
+      }
+    />
+    {subMenu}
+  </Menu.Item>
 );
