c4f52dd9ee57187283e3fa8c27a2f235dd6a19ba
SuperCheese21 <ethan.shields21@gmail.com>
2023-04-19 18:30:02 -0700

    Added version and build number to footer

--- resources/resources.d.ts
+++ resources/resources.d.ts
@@ -1,3 +1,6 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
 
 /// <reference types="vite/client" />
+
+declare const APP_VERSION: string;
+declare const APP_BUILD_NUMBER: string;
--- vitest/setupTests.ts
+++ vitest/setupTests.ts
@@ -1,6 +1,9 @@
 import createFetchMock from 'vitest-fetch-mock';
 import { vi } from 'vitest';
 
+vi.stubGlobal('APP_VERSION', '0.0.1');
+vi.stubGlobal('APP_BUILD_NUMBER', '1234567');
+
 const fetchMock = createFetchMock(vi);
 
 fetchMock.enableMocks();
