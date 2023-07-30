52bff6455c9910e3f24ca446684747d38f300808
SuperCheese21 <ethan.shields21@gmail.com>
2023-07-29 17:29:13 -0700

    Updated dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import react from '@vitejs/plugin-react';
 import { execSync } from 'child_process';
 import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
 
 const commitHash = execSync('git rev-parse --short HEAD').toString();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version),
     APP_BUILD_NUMBER: JSON.stringify(commitHash),
