b2148de7bad79e1d7b2e95ebb9b70af5e3210a4d
SuperCheese21 <ethan.shields21@gmail.com>
2023-11-12 22:35:29 -0800

    Fix input backgrounds

--- app/db/seeders/seedAirframes.ts
+++ app/db/seeders/seedAirframes.ts
@@ -115,7 +115,7 @@ const updateAirframe = async (
   console.log('Seeding airframes...');
   try {
     const data = fs
-      .readFileSync('./app/db/seeders/data/aircraftDatabase-2023-08.csv')
+      .readFileSync('./app/db/seeders/data/aircraftDatabase-2023-11.csv')
       .toString();
     const rows = getDatabaseRows(data);
     console.log(`Attempting to add ${rows.length} airframes`);
