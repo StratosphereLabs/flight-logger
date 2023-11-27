ec824b909b730a952348f5968b40414eb31e7dea
SuperCheese21 <ethan.shields21@gmail.com>
2023-11-27 11:52:57 +0800

    Fix airline seeder

--- app/db/seeders/constants.ts
+++ app/db/seeders/constants.ts
@@ -4,5 +4,5 @@ export const WIKI_PROMISE_CONCURRENCY = 5;
 
 export const FREIGHTER_AIRCRAFT_REGEX = /F$|Freighter|sharklets/i;
 
-export const IATA_AIRLINE_CODE_REGEX = /[0-9][A-Z]{2}/;
+export const IATA_AIRLINE_CODE_REGEX = /[A-Z0-9]{2}/;
 export const ICAO_AIRLINE_CODE_REGEX = /[A-Z]{3}/;
