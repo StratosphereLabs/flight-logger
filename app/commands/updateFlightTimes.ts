e117e92f77ef5e811576bb23ee45a16c1d18b9a1
SuperCheese21 <ethan.shields21@gmail.com>
2023-11-29 14:44:46 +0800

    Changed script to update all flights within 12 hours

--- app/commands/updateFlightTimes.ts
+++ app/commands/updateFlightTimes.ts
@@ -1,8 +1,7 @@
+import type { airline } from '@prisma/client';
 import axios from 'axios';
 import cheerio from 'cheerio';
-import { differenceInMinutes } from 'date-fns';
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
+import { add, differenceInMinutes, sub } from 'date-fns';
 import { prisma } from '../db';
 import type { FlightAwareDataResponse } from './types';
 import { createNewDate } from './utils';
@@ -10,10 +9,10 @@ import { createNewDate } from './utils';
 const SCRIPT_BEGIN = 'var trackpollBootstrap = ';
 
 export const fetchFlightAwareData = async (
-  airlineIcao: string,
+  airline: airline,
   flightNumber: number,
 ): Promise<FlightAwareDataResponse | null> => {
-  const url = `https://www.flightaware.com/live/flight/${airlineIcao}${flightNumber}`;
+  const url = `https://www.flightaware.com/live/flight/${airline.icao}${flightNumber}`;
   const response = await axios.get<string>(url);
   const $ = cheerio.load(response.data);
   let flightData: FlightAwareDataResponse | null = null;
@@ -29,80 +28,77 @@ export const fetchFlightAwareData = async (
 };
 
 export const updateFlightTimes = async (): Promise<void> => {
-  const argv = await yargs(hideBin(process.argv)).parse();
-  const flightId = argv._[0] as string;
-  const flightData = await prisma.flight.findUnique({
-    where: { id: flightId },
+  const flights = await prisma.flight.findMany({
+    where: {
+      outTime: {
+        gt: sub(new Date(), { hours: 12 }),
+        lt: add(new Date(), { hours: 12 }),
+      },
+    },
     include: {
       airline: true,
     },
   });
-  if (flightData === null) {
-    console.error('Flight not found.');
-    process.exit(1);
+  if (flights.length === 0) {
+    console.log('No flights to update.');
+    process.exit(0);
   }
-  if (flightData.airline === null) {
-    console.error('Missing airline.');
-    process.exit(1);
+  for (const flightData of flights) {
+    console.log(`Updating flight ${flightData.id}...`);
+    if (flightData.airline === null) {
+      continue;
+    }
+    if (flightData.flightNumber === null) {
+      continue;
+    }
+    const data = await fetchFlightAwareData(
+      flightData.airline,
+      flightData.flightNumber,
+    );
+    if (data === null) {
+      console.error('  Unable to fetch flight data. Please try again later.');
+      continue;
+    }
+    const flightAwareFlightData = Object.values(data.flights)[0];
+    const outTimeScheduled = createNewDate(
+      flightAwareFlightData.gateDepartureTimes.scheduled,
+    );
+    const timeDiff = Math.abs(
+      differenceInMinutes(flightData.outTime, outTimeScheduled),
+    );
+    if (timeDiff >= 720) {
+      console.error('  Flight must be within 12 hours.');
+      continue;
+    }
+    await prisma.flight.update({
+      where: {
+        id: flightData.id,
+      },
+      data: {
+        outTime: outTimeScheduled,
+        outTimeActual: createNewDate(
+          flightAwareFlightData.gateDepartureTimes.actual ??
+            flightAwareFlightData.gateDepartureTimes.estimated,
+        ),
+        offTime: createNewDate(flightAwareFlightData.takeoffTimes.scheduled),
+        offTimeActual: createNewDate(
+          flightAwareFlightData.takeoffTimes.actual ??
+            flightAwareFlightData.takeoffTimes.estimated,
+        ),
+        onTime: createNewDate(flightAwareFlightData.landingTimes.scheduled),
+        onTimeActual: createNewDate(
+          flightAwareFlightData.landingTimes.actual ??
+            flightAwareFlightData.landingTimes.estimated,
+        ),
+        inTime: createNewDate(flightAwareFlightData.gateArrivalTimes.scheduled),
+        inTimeActual: createNewDate(
+          flightAwareFlightData.gateArrivalTimes.actual ??
+            flightAwareFlightData.gateArrivalTimes.estimated,
+        ),
+      },
+    });
   }
-  if (flightData.flightNumber === null) {
-    console.error('Missing flight number.');
-    process.exit(1);
-  }
-  const minutesFromNow = Math.abs(
-    differenceInMinutes(new Date(), flightData.outTime),
-  );
-  if (minutesFromNow >= 720) {
-    console.error('Flight must be within 12 hours.');
-    process.exit(1);
-  }
-  const data = await fetchFlightAwareData(
-    flightData.airline.icao,
-    flightData.flightNumber,
-  );
-  if (data === null) {
-    console.error('Unable to fetch flight data. Please try again later.');
-    process.exit(1);
-  }
-  const flightAwareFlightData = Object.values(data.flights)[0];
-  const outTimeScheduled = createNewDate(
-    flightAwareFlightData.gateDepartureTimes.scheduled,
-  );
-  const timeDiff = Math.abs(
-    differenceInMinutes(flightData.outTime, outTimeScheduled),
-  );
-  if (timeDiff >= 720) {
-    console.error('Flight must be within 12 hours.');
-    process.exit(1);
-  }
-  await prisma.flight.update({
-    where: {
-      id: flightId,
-    },
-    data: {
-      outTime: outTimeScheduled,
-      outTimeActual: createNewDate(
-        flightAwareFlightData.gateDepartureTimes.actual ??
-          flightAwareFlightData.gateDepartureTimes.estimated,
-      ),
-      offTime: createNewDate(flightAwareFlightData.takeoffTimes.scheduled),
-      offTimeActual: createNewDate(
-        flightAwareFlightData.takeoffTimes.actual ??
-          flightAwareFlightData.takeoffTimes.estimated,
-      ),
-      onTime: createNewDate(flightAwareFlightData.landingTimes.scheduled),
-      onTimeActual: createNewDate(
-        flightAwareFlightData.landingTimes.actual ??
-          flightAwareFlightData.landingTimes.estimated,
-      ),
-      inTime: createNewDate(flightAwareFlightData.gateArrivalTimes.scheduled),
-      inTimeActual: createNewDate(
-        flightAwareFlightData.gateArrivalTimes.actual ??
-          flightAwareFlightData.gateArrivalTimes.estimated,
-      ),
-    },
-  });
-  console.log('Flight updated successfully.');
+  console.log('  Flight updated successfully.');
 };
 
 (() => {
