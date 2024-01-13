04a88bace09448e6c6e4e029b016fa98ea308b36
SuperCheese21 <ethan.shields21@gmail.com>
2024-01-12 21:55:25 -0800

    Added URL state for fullscreen map

--- resources/pages/Profile/Profile.tsx
+++ resources/pages/Profile/Profile.tsx
@@ -1,11 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { CompletedFlights } from './CompletedFlights';
 import { MapCard } from './MapCard';
 import { ProfileCard } from './ProfileCard';
 import { UpcomingFlights } from './UpcomingFlights';
 
 export const Profile = (): JSX.Element => {
-  const [isMapFullScreen, setIsMapFullScreen] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [initialParams] = useState(searchParams);
+  const [isMapFullScreen, setIsMapFullScreen] = useState(
+    initialParams.get('isMapFullScreen') === 'true',
+  );
+  useEffect(() => {
+    setSearchParams(oldSearchParams => ({
+      ...Object.fromEntries(oldSearchParams),
+      isMapFullScreen: isMapFullScreen.toString(),
+    }));
+  }, [isMapFullScreen, setSearchParams]);
   return (
     <div className="flex flex-row flex-wrap gap-4">
       {!isMapFullScreen ? <ProfileCard /> : null}
