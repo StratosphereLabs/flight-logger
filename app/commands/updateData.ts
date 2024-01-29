479a2f51f7100f027a6ee28f28be3fba855bb0d1
SuperCheese21 <ethan.shields21@gmail.com>
2024-01-29 13:23:46 -0800

    Added new job to update current flights every 5 minutes

--- app/commands/updateData.ts
+++ app/commands/updateData.ts
@@ -1,5 +1,5 @@
 import { Promise } from 'bluebird';
-import { add, sub } from 'date-fns';
+import { add, isAfter, isBefore, sub } from 'date-fns';
 import groupBy from 'lodash.groupby';
 import { scheduleJob } from 'node-schedule';
 import { prisma } from '../db';
@@ -11,9 +11,40 @@ import {
   seedManufacturers,
 } from '../db/seeders';
 import { UPDATE_CONCURRENCY } from './constants';
+import type { FlightWithData } from './types';
 import { updateFlightTimesData } from './updateFlightTimesData';
 import { updateFlightRegistrationData } from './updateFlightRegistrationData';
 import { getGroupedFlightsKey } from './utils';
+
+const processFlightUpdate = async (
+  flightsToUpdate: FlightWithData[],
+): Promise<void> => {
+  if (flightsToUpdate.length === 0) {
+    console.log('No flights to update.');
+    return;
+  }
+  const groupedFlights = groupBy(flightsToUpdate, getGroupedFlightsKey);
+  try {
+    await Promise.map(
+      Object.entries(groupedFlights),
+      async ([key, flights]) => {
+        console.log(`Updating flight ${key}...`);
+        await updateFlightTimesData(flights);
+        await updateFlightRegistrationData(flights);
+      },
+      {
+        concurrency: UPDATE_CONCURRENCY,
+      },
+    );
+    console.log(
+      `  ${flightsToUpdate.length} flight${
+        flightsToUpdate.length > 1 ? 's' : ''
+      } updated successfully.`,
+    );
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 const updateFlights = async (): Promise<void> => {
   const flightsToUpdate = await prisma.flight.findMany({
@@ -71,35 +102,62 @@ const updateFlights = async (): Promise<void> => {
       },
     },
   });
-  if (flightsToUpdate.length === 0) {
-    console.log('No flights to update.');
-    return;
-  }
-  const groupedFlights = groupBy(flightsToUpdate, getGroupedFlightsKey);
-  try {
-    await Promise.map(
-      Object.entries(groupedFlights),
-      async ([key, flights]) => {
-        console.log(`Updating flight ${key}...`);
-        await updateFlightTimesData(flights);
-        await updateFlightRegistrationData(flights);
+  await processFlightUpdate(flightsToUpdate);
+};
+
+const updateCurrentFlights = async (): Promise<void> => {
+  const flightsToUpdate = await prisma.flight.findMany({
+    where: {
+      OR: [
+        {
+          inTimeActual: {
+            gt: sub(new Date(), { minutes: 30 }),
+            lte: add(new Date(), { minutes: 90 }),
+          },
+        },
+        {
+          inTime: {
+            gt: sub(new Date(), { minutes: 30 }),
+            lte: add(new Date(), { minutes: 90 }),
+          },
+        },
+      ],
+      airline: {
+        isNot: null,
       },
-      {
-        concurrency: UPDATE_CONCURRENCY,
+      flightNumber: {
+        not: null,
       },
+    },
+    include: {
+      airline: true,
+      departureAirport: {
+        select: {
+          iata: true,
+          timeZone: true,
+        },
+      },
+      arrivalAirport: {
+        select: {
+          iata: true,
+          timeZone: true,
+        },
+      },
+    },
+  });
+  const filteredFlights = flightsToUpdate.filter(({ inTime, inTimeActual }) => {
+    const arrivalTime = inTimeActual ?? inTime;
+    return (
+      isAfter(arrivalTime, sub(new Date(), { minutes: 30 })) &&
+      isBefore(arrivalTime, add(new Date(), { minutes: 90 }))
     );
-    console.log(
-      `  ${flightsToUpdate.length} flight${
-        flightsToUpdate.length > 1 ? 's' : ''
-      } updated successfully.`,
-    );
-  } catch (err) {
-    console.error(err);
-  }
+  });
+  await processFlightUpdate(filteredFlights);
 };
 
 (() => {
   scheduleJob('*/15 * * * *', updateFlights);
+  scheduleJob('5,10,20,25,35,40,50,55 * * * *', updateCurrentFlights);
   scheduleJob('0 0 1 * *', async () => {
     await seedManufacturers();
     await seedAircraftTypes();
