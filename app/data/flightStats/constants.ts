ae8495d34e187e353a0d6c36d8ce9738ceac29f6
SuperCheese21 <ethan.shields21@gmail.com>
2024-01-30 16:35:19 -0800

    Added column for custom airline codes

--- app/data/flightStats/constants.ts
+++
@@ -1,4 +0,0 @@
-export const OVERRIDE_CODES: Record<string, string> = {
-  JX: 'J2X',
-  '4Y': '4Y*',
-};
---
+++ prisma/migrations/20240131000857_added_flightstats_override_code_to_airlines_table/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "airline" ADD COLUMN     "flightStatsCode" TEXT;
