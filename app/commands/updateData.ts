426023b405bb9dffe15797601cfec7a078f5fcbf
SuperCheese21 <ethan.shields21@gmail.com>
2024-02-10 11:39:54 -0800

    Increase flight update frequency near departure time

--- app/commands/updateData.ts
+++ app/commands/updateData.ts
@@ -111,6 +111,18 @@ const updateCurrentFlights = async (): Promise<void> => {
     where: {
       OR: [
         {
+          outTimeActual: {
+            gt: sub(new Date(), { minutes: 30 }),
+            lte: add(new Date(), { minutes: 90 }),
+          },
+        },
+        {
+          outTime: {
+            gt: sub(new Date(), { minutes: 30 }),
+            lte: add(new Date(), { minutes: 90 }),
+          },
+        },
+        {
           inTimeActual: {
             gt: sub(new Date(), { minutes: 30 }),
             lte: add(new Date(), { minutes: 90 }),
