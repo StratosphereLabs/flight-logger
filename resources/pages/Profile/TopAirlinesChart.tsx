a42e43cea2c4af9755a18ee9739411814d3a33b6
SuperCheese21 <ethan.shields21@gmail.com>
2024-02-09 21:40:40 -0800

    Added loading spinner to charts

--- resources/pages/Profile/TopAirlinesChart.tsx
+++ resources/pages/Profile/TopAirlinesChart.tsx
@@ -1,7 +1,8 @@
 import { ResponsiveBar } from '@nivo/bar';
+import classNames from 'classnames';
 import { useForm, useWatch } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
-import { Form, Select } from 'stratosphere-ui';
+import { Form, Loading, Select } from 'stratosphere-ui';
 import { trpc } from '../../utils/trpc';
 import { BAR_CHART_THEME } from './constants';
 
@@ -20,15 +21,20 @@ export const TopAirlinesChart = (): JSX.Element => {
     name: 'mode',
     control: methods.control,
   });
-  const { data } = trpc.statistics.getTopAirlines.useQuery({
-    username,
-    limit: 5,
-    mode,
-  });
+  const { data, isFetching } = trpc.statistics.getTopAirlines.useQuery(
+    {
+      username,
+      limit: 5,
+      mode,
+    },
+    {
+      keepPreviousData: true,
+    },
+  );
   return (
-    <div className="flex min-w-[250px] max-w-[500px] flex-1 flex-col items-center gap-1 font-semibold">
+    <div className="flex h-[180px] min-w-[250px] max-w-[500px] flex-1 flex-col items-center gap-1 font-semibold">
       <div className="flex h-9 w-full items-center justify-between">
-        <div className="text-sm font-semibold">Top Airlines</div>
+        <div className="text-sm">Top Airlines</div>
         <Form methods={methods}>
           <Select
             buttonProps={{ color: 'ghost', size: 'xs' }}
@@ -49,35 +55,47 @@ export const TopAirlinesChart = (): JSX.Element => {
               },
             ]}
             menuSize="sm"
-            menuClassName="w-[175px]"
+            menuClassName="w-[175px] right-0"
             name="mode"
           />
         </Form>
       </div>
-      {data !== undefined ? (
-        <div className="h-[150px] w-full min-w-0">
-          <ResponsiveBar
-            theme={BAR_CHART_THEME}
-            layout="horizontal"
-            data={data}
-            keys={[mode]}
-            indexBy="airline"
-            enableGridY={false}
-            axisBottom={{
-              tickSize: 0,
-            }}
-            axisLeft={{
-              tickSize: 0,
-              tickPadding: 10,
-            }}
-            margin={{
-              left: 55,
-            }}
-            colors={['var(--fallback-in,oklch(var(--in)/0.75))']}
-            borderColor="#000000"
-          />
-        </div>
-      ) : null}
+      <div className="relative h-full w-full">
+        {isFetching ? (
+          <div className="absolute flex h-full w-full items-center justify-center">
+            <Loading shape="spinner" />
+          </div>
+        ) : null}
+        {data !== undefined ? (
+          <div
+            className={classNames(
+              'h-full w-full transition-opacity',
+              isFetching && 'opacity-50',
+            )}
+          >
+            <ResponsiveBar
+              theme={BAR_CHART_THEME}
+              layout="horizontal"
+              data={data}
+              keys={[mode]}
+              indexBy="airline"
+              enableGridY={false}
+              axisBottom={{
+                tickSize: 0,
+              }}
+              axisLeft={{
+                tickSize: 0,
+                tickPadding: 10,
+              }}
+              margin={{
+                left: 55,
+              }}
+              colors={['var(--fallback-in,oklch(var(--in)/0.75))']}
+              borderColor="#000000"
+            />
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 };
--- resources/pages/Profile/TopAirportsChart.tsx
+++ resources/pages/Profile/TopAirportsChart.tsx
@@ -1,42 +1,61 @@
 import { ResponsiveBar } from '@nivo/bar';
+import classNames from 'classnames';
 import { useParams } from 'react-router-dom';
+import { Loading } from 'stratosphere-ui';
 import { trpc } from '../../utils/trpc';
 import { BAR_CHART_THEME } from './constants';
 
 export const TopAirportsChart = (): JSX.Element => {
   const { username } = useParams();
-  const { data } = trpc.statistics.getTopAirports.useQuery({
-    username,
-    limit: 5,
-  });
+  const { data, isFetching } = trpc.statistics.getTopAirports.useQuery(
+    {
+      username,
+      limit: 5,
+    },
+    {
+      keepPreviousData: true,
+    },
+  );
   return (
-    <div className="flex min-w-[250px] max-w-[500px] flex-1 flex-col items-center gap-1 font-semibold">
+    <div className="flex h-[180px] min-w-[250px] max-w-[500px] flex-1 flex-col items-center gap-1 font-semibold">
       <div className="flex h-9 w-full items-center justify-between">
         <div className="text-sm">Top Airports</div>
       </div>
-      {data !== undefined ? (
-        <div className="h-[150px] w-full">
-          <ResponsiveBar
-            theme={BAR_CHART_THEME}
-            layout="horizontal"
-            data={data}
-            keys={['flights']}
-            indexBy="airport"
-            enableGridY={false}
-            axisBottom={{
-              tickSize: 0,
-            }}
-            axisLeft={{
-              tickSize: 0,
-              tickPadding: 15,
-            }}
-            margin={{
-              left: 55,
-            }}
-            colors={['var(--fallback-su,oklch(var(--su)/0.75))']}
-          />
-        </div>
-      ) : null}
+      <div className="relative h-full w-full">
+        {isFetching ? (
+          <div className="absolute flex h-full w-full items-center justify-center">
+            <Loading shape="spinner" />
+          </div>
+        ) : null}
+        {data !== undefined ? (
+          <div
+            className={classNames(
+              'h-full w-full transition-opacity',
+              isFetching && 'opacity-50',
+            )}
+          >
+            <ResponsiveBar
+              theme={BAR_CHART_THEME}
+              layout="horizontal"
+              data={data}
+              keys={['flights']}
+              indexBy="airport"
+              enableGridY={false}
+              axisBottom={{
+                tickSize: 0,
+              }}
+              axisLeft={{
+                tickSize: 0,
+                tickPadding: 15,
+              }}
+              margin={{
+                left: 55,
+              }}
+              colors={['var(--fallback-su,oklch(var(--su)/0.75))']}
+            />
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 };
