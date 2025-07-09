c63d9793c1c570c189c91da331da60a403ac6f2c
SuperCheese21 <ethan.shields21@gmail.com>
2025-07-08 21:01:09 -0700

    Added historical weather for flights within 60 hours ago

--- resources/common/hooks/useWeatherRadarLayer.ts
+++ resources/common/hooks/useWeatherRadarLayer.ts
@@ -9,18 +9,24 @@ import {
 
 export const useWeatherRadarLayer = (
   map: google.maps.Map | null,
+  timestamp?: number | null,
 ): UseQueryResult<AxiosResponse<RainviewerApiResult>> => {
   const queryResult = useRainviewerApiQuery();
   useEffect(() => {
     if (
       map !== null &&
+      timestamp !== null &&
       queryResult.data !== undefined &&
       map.overlayMapTypes.getLength() < 2
     ) {
       const weatherRadarMapType = new google.maps.ImageMapType({
         getTileUrl: (coord, zoom) => {
           const { past } = queryResult.data.data.radar;
-          return `https://cdn.rainviewer.com${past[past.length - 1].path}/256/${zoom}/${coord.x}/${coord.y}/2/1_1.png`;
+          const path =
+            timestamp !== undefined
+              ? `/v2/radar/${timestamp}`
+              : past[past.length - 1].path;
+          return `https://cdn.rainviewer.com${path}/256/${zoom}/${coord.x}/${coord.y}/2/1_1.png`;
         },
         tileSize: new google.maps.Size(256, 256),
         maxZoom: 9,
@@ -30,6 +36,6 @@ export const useWeatherRadarLayer = (
       });
       map.overlayMapTypes.push(weatherRadarMapType);
     }
-  }, [map, queryResult.data]);
+  }, [map, queryResult.data, timestamp]);
   return queryResult;
 };
