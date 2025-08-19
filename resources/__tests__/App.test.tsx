6e32e4527e3d6664bcd462eb2b1221c48ca8e1d1
SuperCheese21 <ethan.shields21@gmail.com>
2025-08-18 22:28:23 -0800

    Mock nivo in unit tests

--- resources/__tests__/App.test.tsx
+++ resources/__tests__/App.test.tsx
@@ -1,7 +1,23 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 
 import { AppRouter } from '../AppRouter';
 import { render } from '../common/test-utils';
+
+vi.mock('@nivo/bar', () => ({
+  ResponsiveBar: () => <div data-testid="mock-bar-chart" />,
+}));
+
+vi.mock('@nivo/line', () => ({
+  ResponsiveLine: () => <div data-testid="mock-line-chart" />,
+}));
+
+vi.mock('@nivo/pie', () => ({
+  ResponsivePie: () => <div data-testid="mock-pie-chart" />,
+}));
+
+vi.mock('@nivo/radar', () => ({
+  ResponsiveRadar: () => <div data-testid="mock-radar-chart" />,
+}));
 
 describe.skip('<App />', () => {
   it('renders login page', () => {
--- vitest/setupTests.ts
+++
@@ -1,9 +0,0 @@
-import createFetchMock from 'vitest-fetch-mock';
-import { vi } from 'vitest';
-
-vi.stubGlobal('APP_VERSION', '0.0.1');
-vi.stubGlobal('APP_BUILD_NUMBER', '1234567');
-
-const fetchMock = createFetchMock(vi);
-
-fetchMock.enableMocks();
