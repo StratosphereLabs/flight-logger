bc3c0868c4f32f4d9bff352088d266e67df7af67
SuperCheese21 <ethan.shields21@gmail.com>
2025-12-31 19:50:22 -0800

    Moved Statsig wrapper to AppWrapper component

--- resources/AppRouter.tsx
+++ resources/AppRouter.tsx
@@ -1,13 +1,6 @@
-import {
-  StatsigProvider,
-  useClientAsyncInit,
-  useStatsigUser,
-} from '@statsig/react-bindings';
-import { StatsigSessionReplayPlugin } from '@statsig/session-replay';
-import { StatsigAutoCapturePlugin } from '@statsig/web-analytics';
+import { useStatsigUser } from '@statsig/react-bindings';
 import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Loading } from 'stratosphere-ui';
 
 import { useLoggedInUserQuery } from './common/hooks';
 import { AuthenticationLayout, MainLayout, ProfileLayout } from './layouts';
@@ -30,16 +23,6 @@ import { useProfileFilterForm } from './pages/Profile/hooks';
 export const AppRouter = (): JSX.Element => {
   const methods = useProfileFilterForm();
   const { data } = useLoggedInUserQuery();
-  const { client } = useClientAsyncInit(
-    import.meta.env.VITE_STATSIG_API_KEY as string,
-    {},
-    {
-      plugins: [
-        new StatsigAutoCapturePlugin(),
-        new StatsigSessionReplayPlugin(),
-      ],
-    },
-  );
   const { updateUserSync } = useStatsigUser();
   useEffect(() => {
     if (data !== undefined) {
@@ -50,34 +33,32 @@ export const AppRouter = (): JSX.Element => {
     }
   }, [data, updateUserSync]);
   return (
-    <StatsigProvider client={client} loadingComponent={<Loading />}>
-      <Routes>
-        <Route path="/" element={<MainLayout methods={methods} />}>
-          <Route path="" element={<Home />} />
-          <Route path="data" element={<Data />} />
-          <Route path="users" element={<Users />} />
-          {['', 'user/:username'].map(path => (
-            <Route key={path} path={path} element={<ProfileLayout />}>
-              <Route
-                path={path === '' ? 'profile' : ''}
-                element={<Profile filtersFormControl={methods.control} />}
-              />
-              <Route path="trips/:tripId?" element={<Trips />} />
-              {path === '' ? (
-                <Route path="account" element={<Account />} />
-              ) : null}
-            </Route>
-          ))}
-          <Route path="flight/:flightId" element={<Flight />} />
-          <Route path="aircraft/:icao24" element={<Aircraft />} />
-        </Route>
-        <Route path="auth" element={<AuthenticationLayout />}>
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="forgot-password" element={<ForgotPassword />} />
-          <Route path="reset-password/:token" element={<ResetPassword />} />
-        </Route>
-      </Routes>
-    </StatsigProvider>
+    <Routes>
+      <Route path="/" element={<MainLayout methods={methods} />}>
+        <Route path="" element={<Home />} />
+        <Route path="data" element={<Data />} />
+        <Route path="users" element={<Users />} />
+        {['', 'user/:username'].map(path => (
+          <Route key={path} path={path} element={<ProfileLayout />}>
+            <Route
+              path={path === '' ? 'profile' : ''}
+              element={<Profile filtersFormControl={methods.control} />}
+            />
+            <Route path="trips/:tripId?" element={<Trips />} />
+            {path === '' ? (
+              <Route path="account" element={<Account />} />
+            ) : null}
+          </Route>
+        ))}
+        <Route path="flight/:flightId" element={<Flight />} />
+        <Route path="aircraft/:icao24" element={<Aircraft />} />
+      </Route>
+      <Route path="auth" element={<AuthenticationLayout />}>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="forgot-password" element={<ForgotPassword />} />
+        <Route path="reset-password/:token" element={<ResetPassword />} />
+      </Route>
+    </Routes>
   );
 };
