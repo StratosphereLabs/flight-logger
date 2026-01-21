05e23bbd72a100fe8f5ceb152b527df23e58a4fc
SuperCheese21 <ethan.shields21@gmail.com>
2026-01-20 18:14:48 -0800

    Updated package, added tanstack router devtools

--- resources/layouts/MainLayout/MainLayout.tsx
+++ resources/layouts/MainLayout/MainLayout.tsx
@@ -1,5 +1,6 @@
 import { useGateValue } from '@statsig/react-bindings';
 import { Outlet, useParams } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import classNames from 'classnames';
 import { useEffect, useRef, useState } from 'react';
 import { type UseFormReturn } from 'react-hook-form';
@@ -48,58 +49,63 @@ export const MainLayout = (): JSX.Element => {
     scrollContainerRef.current?.scrollTo(0, 0);
   }, [username]);
   return (
-    <div className="relative flex h-[100dvh] flex-col justify-between">
-      {theme === AppTheme.CHRISTMAS && christmasThemeEnabled ? (
-        <Snowfall style={{ zIndex: 50 }} />
-      ) : null}
-      <MainNavbar methods={methods} />
-      <div
-        className="bg-base-200 flex flex-1 flex-col justify-between overflow-x-hidden overflow-y-scroll"
-        ref={scrollContainerRef}
-      >
-        <Outlet />
-        <MainFooter />
+    <>
+      <div className="relative flex h-[100dvh] flex-col justify-between">
+        {theme === AppTheme.CHRISTMAS && christmasThemeEnabled ? (
+          <Snowfall style={{ zIndex: 50 }} />
+        ) : null}
+        <MainNavbar methods={methods} />
+        <div
+          className="bg-base-200 flex flex-1 flex-col justify-between overflow-x-hidden overflow-y-scroll"
+          ref={scrollContainerRef}
+        >
+          <Outlet />
+          <MainFooter />
+        </div>
+        {alertMessages.length > 0 ? (
+          <div className="toast toast-end toast-top z-50 w-1/2 min-w-[400px]">
+            <AlertMessages maxMessages={4} />
+          </div>
+        ) : null}
+        {theme === AppTheme.CHRISTMAS &&
+        christmasThemeEnabled &&
+        showSnowbank ? (
+          <div className="pointer-events-none fixed bottom-0 left-0 h-[70px] w-full overflow-hidden">
+            <div className="animate-rise absolute bottom-0 h-[70px] w-full rounded-t-[30%] bg-[#dee4fd]"></div>
+          </div>
+        ) : null}
+        {theme === AppTheme.CHRISTMAS && christmasThemeEnabled ? (
+          <>
+            <footer
+              className={classNames(
+                'footer text-info fixed bottom-0 left-0 flex w-full justify-between overflow-hidden p-5 brightness-75 transition-opacity',
+                showFooterText
+                  ? 'pointer-events-auto opacity-100'
+                  : 'pointer-events-none opacity-0',
+              )}
+            >
+              <div className="truncate">
+                <p>
+                  <span className="hidden sm:inline-block">Copyright</span> ©{' '}
+                  {new Date().getFullYear()}{' '}
+                  <a
+                    className="link-hover link"
+                    href="https://github.com/StratosphereLabs"
+                  >
+                    Stratosphere Labs
+                  </a>
+                </p>
+              </div>
+              <div className="flex gap-1 truncate opacity-75">
+                <span className="hidden sm:inline-block">Version</span>{' '}
+                {APP_VERSION}{' '}
+                <span className="opacity-50">{APP_BUILD_NUMBER}</span>
+              </div>
+            </footer>
+          </>
+        ) : null}
       </div>
-      {alertMessages.length > 0 ? (
-        <div className="toast toast-end toast-top z-50 w-1/2 min-w-[400px]">
-          <AlertMessages maxMessages={4} />
-        </div>
-      ) : null}
-      {theme === AppTheme.CHRISTMAS && christmasThemeEnabled && showSnowbank ? (
-        <div className="pointer-events-none fixed bottom-0 left-0 h-[70px] w-full overflow-hidden">
-          <div className="animate-rise absolute bottom-0 h-[70px] w-full rounded-t-[30%] bg-[#dee4fd]"></div>
-        </div>
-      ) : null}
-      {theme === AppTheme.CHRISTMAS && christmasThemeEnabled ? (
-        <>
-          <footer
-            className={classNames(
-              'footer text-info fixed bottom-0 left-0 flex w-full justify-between overflow-hidden p-5 brightness-75 transition-opacity',
-              showFooterText
-                ? 'pointer-events-auto opacity-100'
-                : 'pointer-events-none opacity-0',
-            )}
-          >
-            <div className="truncate">
-              <p>
-                <span className="hidden sm:inline-block">Copyright</span> ©{' '}
-                {new Date().getFullYear()}{' '}
-                <a
-                  className="link-hover link"
-                  href="https://github.com/StratosphereLabs"
-                >
-                  Stratosphere Labs
-                </a>
-              </p>
-            </div>
-            <div className="flex gap-1 truncate opacity-75">
-              <span className="hidden sm:inline-block">Version</span>{' '}
-              {APP_VERSION}{' '}
-              <span className="opacity-50">{APP_BUILD_NUMBER}</span>
-            </div>
-          </footer>
-        </>
-      ) : null}
-    </div>
+      <TanStackRouterDevtools />
+    </>
   );
 };
